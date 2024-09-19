7ab521eb701dd88414ddd37cc839bf66ec1cd1a3
Kevin <kevincarr@gmail.com>
2024-09-19 14:21:06 -0300

    more statistics

--- helpers.py
+++ helpers.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 
 
 def sql(query, dialect='mysql', driver='mysqlconnector', username='root', password='root', host='localhost',
-        port='3306', database='dmapps'):
+        port='3306', database='dmapps_0'):
     engine = create_engine(f'{dialect}+{driver}://{username}:{password}@{host}:{port}/{database}')
     with engine.connect() as conn:
         return pd.read_sql(query, conn)
