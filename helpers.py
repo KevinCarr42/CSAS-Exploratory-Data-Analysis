7987e225e3716d7e99b2530b53f2a97025b26b65
Kevin <kevincarr@gmail.com>
2025-07-23 11:17:44 -0300

    EDA to investigate app usage and compliance

--- helpers.py
+++ helpers.py
@@ -3,18 +3,19 @@ from sqlalchemy import create_engine
 
 
 def sql(query, dialect='mysql', driver='mysqlconnector', username='root', password='root', host='localhost',
-        port='3306', database='dmapps_0'):
+        port='3306', database='dmapps_0', disambiguate=False):
     engine = create_engine(f'{dialect}+{driver}://{username}:{password}@{host}:{port}/{database}')
     with engine.connect() as conn:
         df = pd.read_sql(query, conn)
-        counts = {}
-        new_cols = []
-        for col in df.columns:
-            if col in counts:
-                counts[col] += 1
-                new_cols.append(f"{col}_{counts[col]}")
-            else:
-                counts[col] = 0
-                new_cols.append(col)
-        df.columns = new_cols
+        if disambiguate:
+            counts = {}
+            new_cols = []
+            for col in df.columns:
+                if col in counts:
+                    counts[col] += 1
+                    new_cols.append(f"{col}_{counts[col]}")
+                else:
+                    counts[col] = 0
+                    new_cols.append(col)
+            df.columns = new_cols
         return df
--- queries.sql
+++ queries.sql
@@ -1,10 +1,28 @@
-SELECT * FROM csas2_document
-    JOIN csas2_documenttype ON csas2_document.document_type_id = csas2_documenttype.id;
+SELECT * FROM csas2_csasrequest;
 
 SELECT * FROM csas2_process
     JOIN csas2_csasoffice ON csas2_process.lead_office_id = csas2_csasoffice.id
     JOIN shared_models_region ON csas2_csasoffice.region_id = shared_models_region.id;
 
+SELECT * FROM csas2_csasoffice;
+SELECT * FROM shared_models_region;
 
+SELECT * FROM csas2_csasrequestreview;
 
+SELECT * FROM csas2_termsofreference;
 
+SELECT * FROM csas2_meeting;
+
+SELECT * FROM csas2_invitee;
+
+SELECT * FROM csas2_document
+    JOIN csas2_documenttype ON csas2_document.document_type_id = csas2_documenttype.id;
+
+SELECT * FROM csas2_document
+    JOIN csas2_documenttype ON csas2_document.document_type_id = csas2_documenttype.id
+    JOIN csas2_documenttracking ON csas2_documenttracking.document_id = csas2_document.id;
+
+SELECT * FROM csas2_documenttracking LIMIT 1;
+
+SELECT * FROM csas2_changelog;
+SELECT DISTINCT(model_name) FROM csas2_changelog;
